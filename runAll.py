a42fb3fd48ec41c5d2c4d557f7a080dd9c05ecee
Joao Pedro <joopedrof.silva@gmail.com>
2022-10-07 18:35:27 -0300

    Add Cologne scenario and a new scripts
    - Added the file Cologne-JP.zip that describes the scenario of Cologne
    - Now the script runAll.py has the option to select the simulation scenario.
    - Added the script runSome.py that runs EBkSP simulations of specified road congestion models

--- runAll.py
+++ runAll.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-def runthis(num, Chicago):
+def runthis(num, scenario, outputDir):
     count = 0
     reroute = []
     reroute.append("DSP")
@@ -11,47 +11,58 @@ def runthis(num, Chicago):
     while(count < len(reroute)):
         i = 0
         while(i < num):
-            output =("Outputs_%s/reroute_%s_%d.xml"%(reroute[count], reroute[count], i))
+            output =("Outputs_%s/%s/reroute_%s_%d.xml"%(reroute[count], outputDir, reroute[count], i))
             if(reroute[count]) == "DSP":
-                os.system("python %s.py -s %s/sim.sumocfg -n %s/sim.net.xml -i 900 -b 1000 -e 10000 -d %d -o %s -l logs/%s.log"%(reroute[count], Chicago, Chicago, i, output, reroute[count]))
+                os.system("python %s.py -s %s/sim.sumocfg -n %s/sim.net.xml -i 900 -b 1000 -e 10000 -d %d -o %s -l logs/%s.log"%(reroute[count], scenario, scenario, i, output, reroute[count]))
                 i = i+1   
             else: 
-                os.system("python %s.py -s %s/sim.sumocfg -n %s/sim.net.xml -k 3 -i 900 -b 1000 -e 10000 -d %d -o %s -l logs/%s.log"%(reroute[count], Chicago, Chicago, i, output, reroute[count]))
+                os.system("python %s.py -s %s/sim.sumocfg -n %s/sim.net.xml -k 3 -i 900 -b 1000 -e 10000 -d %d -o %s -l logs/%s.log"%(reroute[count], scenario, scenario, i, output, reroute[count]))
                 i = i+1
         print("End simulation of %s"%(reroute[count]))
         count = count + 1
     print("End of all %d simulations"%(i))
 
-
 if __name__ == '__main__':
     root = os.getcwd()
     Chicago = ("%s/Chicago"%(root))
+    Cologne = ("%s/Cologne"%(root))
     num = int(sys.argv[1])
+
+    print("Select the scenario to simulate:\n1.Chicago\t2.Cologne")
+    sce_btn = int(input())
+    scenario = Cologne
+    outputDir = "Cologne"
+    if(sce_btn == 1):
+        scenario = Chicago
+        outputDir = "Chicago"
+    elif(sce_btn == 2):
+        scenario = Cologne
+        outputDir = "Cologne"
     print("Simulating the linear models\n")
     print("Greenshield\n")
     new_path = ("%s/Linear_models/Greenshield/"%(root))
     os.chdir(new_path)
-    runthis(num, Chicago)
+    runthis(num, scenario, outputDir)
     print("Other\n")
     new_path = ("%s/Linear_models/Other/"%(root))
     os.chdir(new_path)
-    runthis(num, Chicago)
+    runthis(num, scenario, outputDir)
     print("Simulating the logarithm models\n")
     print("Drake\n")
     new_path = ("%s/Logarithm_models/Drake/"%(root))
     os.chdir(new_path)
-    runthis(num, Chicago)
+    runthis(num, scenario, outputDir)
     print("Greenberg\n")
     new_path = ("%s/Logarithm_models/Greenberg/"%(root))
     os.chdir(new_path)
-    runthis(num, Chicago)
+    runthis(num, scenario, outputDir)
     #print("Underwood\n")
     #new_path = ("%s/Logarithm_models/Underwood/"%(root))
     #os.chdir(new_path)
-    #runthis(num, Chicago)
+    #runthis(num, scenario)
     print("Greenberg - Underwood\n")
     new_path = ("%s/Logarithm_models/Greenberg-Underwood/"%(root))
     os.chdir(new_path)
-    runthis(num, Chicago)
+    runthis(num, scenario, outputDir)
     print("Ended all models simulation")
     sys.exit()
