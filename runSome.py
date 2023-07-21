555948a003f4917cb1a05deb07502d16389cd008
Joao Pedro <joopedrof.silva@gmail.com>
2023-07-21 19:42:41 -0300

    RunSome modifications (see readme)

--- runSome.py
+++ runSome.py
@@ -8,9 +8,9 @@ def runEBkSP(num, scenario, outputDir):
     reroute.append("EBkSP")
     while(count < len(reroute)):
         i = 0
-        while(i < num):
+        while(i < 1):
             output =("Outputs_%s/%s/reroute_%s_%d.xml"%(reroute[count], outputDir, reroute[count], i))
-            os.system("python2 %s.py -c sumo -s %s/sim.sumocfg -n %s/sim.net.xml -k 3 -i 900 -b 1000 -e 10000 -d %d -o %s -l logs/%s.log"%(reroute[count], scenario, scenario, i, output, reroute[count]))
+            os.system("python %s.py -c sumo -s %s/sim.sumocfg -n %s/sim.net.xml -k 3 -i 900 -b 1000 -e 10000 -d %d -o %s -l logs/%s.log"%(reroute[count], scenario, scenario, i, output, reroute[count]))
             i = i+1
         print("End simulation of %s"%(reroute[count]))
         count = count + 1
@@ -45,43 +45,18 @@ if __name__ == '__main__':
     Chicago = ("%s/Chicago"%(root))
     Cologne = ("%s/Cologne"%(root))
     num = int(sys.argv[1])
-
-    print("Select the scenario to simulate:\n1.Chicago\t2.Cologne")
-    sce_btn = int(input())
+    selected = []
+    models = ["Greenshield", "Greenberg", "G/U", "Drake"]
+    for argi in range(2, len(sys.argv)):
+        i = sys.argv[argi]
+        i = int(i)
+        print(i)
+        selected.append(models[i])
+        
     scenario = Cologne
     outputDir = "Cologne"
-    if(sce_btn == 1):
-        scenario = Chicago
-        outputDir = "Chicago"
-    elif(sce_btn == 2):
-        scenario = Cologne
-        outputDir = "Cologne"
     
-    models = []
-    btn = -2
-    display = ["1.Greenshield", "2.Greenberg", "3.G/U", "4.Drake"]
-    while(btn != -1):
-        print("Select models to run:\n")
-        for item in display:
-            print(("%s\t")%item)
-        print("0. run")
-        btn = int(input())
-        btn = btn - 1
-        if(btn >= 0):
-            if(display[btn] == "1.Greenshield"):
-                display[btn] = ""
-                models.append("Greenshield")
-            elif(display[btn] == "2.Greenberg"):
-                display[btn] = ""
-                models.append("Greenberg")
-            elif(display[btn] == "3.G/U"):
-                display[btn] = ""
-                models.append("G/U")
-            elif(display[btn] == "4.Drake"):
-                display[btn] = ""
-                models.append("Drake")
-
-    for model in models:
+    for model in selected:
         if(model == "Greenshield"):
             runGreenshield(num, scenario, outputDir, root)
         elif(model == "Greenberg"):
