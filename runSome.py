3f087066259dec3101a6fbf2874e2e039c821ec3
Joao Pedro <joopedrof.silva@gmail.com>
2023-08-31 15:54:22 -0300

    Modified procedure runSome
    - The function runMODEL was extracted from the main, now the only procedure that its called is runEBkSP, and the navegation through directories is made through conditionals in main.
    - Added the parameter model to the function runEBkSP. Now each simulation instance has its own .log file.

--- runSome.py
+++ runSome.py
@@ -2,43 +2,19 @@
 import os
 import sys
 
-def runEBkSP(num, scenario, outputDir):
+def runEBkSP(num, model, scenario, outputDir):
     count = 0
     reroute = []
     reroute.append("EBkSP")
     while(count < len(reroute)):
         i = 0
-        while(i < 1):
+        while(i < num):
             output =("Outputs_%s/%s/reroute_%s_%d.xml"%(reroute[count], outputDir, reroute[count], i))
-            os.system("python %s.py -c sumo -s %s/sim.sumocfg -n %s/sim.net.xml -k 3 -i 900 -b 1000 -e 10000 -d %d -o %s -l logs/%s.log --scale 0.75"%(reroute[count], scenario, scenario, i, output, reroute[count]))
+            os.system("python %s.py -c sumo -s %s/sim.sumocfg -n %s/sim.net.xml -k 3 -i 900 -b 1000 -d %d -o %s -l logs/%s_%s_%d.log --scale 0.75"%(reroute[count], scenario, scenario, i, output, reroute[count], model, num))
             i = i+1
         print("End simulation of %s"%(reroute[count]))
         count = count + 1
     print("End of all %d simulations"%(i))
-
-def runGreenshield(num, scenario, outputDir, root):
-    print("Greenshield\n")
-    new_path = ("%s/Linear_models/Greenshield/"%(root))
-    os.chdir(new_path)
-    runEBkSP(num, scenario, outputDir)
-
-def runDrake(num, scenario, outputDir, root):
-    print("Drake\n")
-    new_path = ("%s/Logarithm_models/Drake/"%(root))
-    os.chdir(new_path)
-    runEBkSP(num, scenario, outputDir)
-
-def runGreenberg(num, scenario, outputDir, root):
-    print("Greenberg\n")
-    new_path = ("%s/Logarithm_models/Greenberg/"%(root))
-    os.chdir(new_path)
-    runEBkSP(num, scenario, outputDir)
-
-def runGU(num, scenario, outputDir, root):
-    print("Greenberg - Underwood\n")
-    new_path = ("%s/Logarithm_models/Greenberg-Underwood/"%(root))
-    os.chdir(new_path)
-    runEBkSP(num, scenario, outputDir)
 
 if __name__ == '__main__':
     root = os.getcwd()
@@ -62,13 +38,28 @@ if __name__ == '__main__':
     
     for model in selected:
         if(model == "Greenshield"):
-            runGreenshield(num, scenario, outputDir, root)
+            print("Greenshield\n")
+            new_path = ("%s/Linear_models/Greenshield/"%(root))
+            os.chdir(new_path)
+            runEBkSP(num, model, scenario, outputDir)
+
         elif(model == "Greenberg"):
-            runGreenberg(num, scenario, outputDir, root)
+            print("Greenberg\n")
+            new_path = ("%s/Logarithm_models/Greenberg/"%(root))
+            os.chdir(new_path)
+            runEBkSP(num, model, scenario, outputDir)
+
         elif(model == "G/U"):
-            runGU(num, scenario, outputDir, root)
+            print("Greenberg - Underwood\n")
+            new_path = ("%s/Logarithm_models/Greenberg-Underwood/"%(root))
+            os.chdir(new_path)
+            runEBkSP(num, model, scenario, outputDir)
+
         elif(model == "Drake"):
-            runDrake(num, scenario, outputDir, root)        
+            print("Drake\n")
+            new_path = ("%s/Logarithm_models/Drake/"%(root))
+            os.chdir(new_path)
+            runEBkSP(num, model, scenario, outputDir)        
         
 
     print("Ended all models simulation")
